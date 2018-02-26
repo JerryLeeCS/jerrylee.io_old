4da75df1907f06ec82b7695f26df6d068f99fa5c
Jerry Lee <JerryDeKo@gmail.com>
2018-02-26 12:42:02 -0800

    changed wsgi.py: DJANGO_SETTINGS_MODULE to production

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -103,11 +103,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': '',
+        'NAME': 'site_db',
     }
 }
 
