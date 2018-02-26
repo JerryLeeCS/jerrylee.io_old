3dfdacf94b96b30fe500629162ea8e8a6c7f1b45
Jerry Lee <JerryDeKo@gmail.com>
2018-02-26 14:26:47 -0800

    changes in production.py with configs and AWS settings

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -1,9 +1,9 @@
 from __future__ import absolute_import, unicode_literals
 
 import os
+import decouple import config
 
-env = os.environ.copy()
-SECRET_KEY = env['SECRET_KEY']
+SECRET_KEY = config('SECRET_KEY')
 
 from .base import *
 
@@ -24,7 +24,6 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 COMPRESS_OFFLINE = True
 COMPRESS_CSS_FILTERS = [
@@ -32,3 +31,18 @@ COMPRESS_CSS_FILTERS = [
     'compressor.filters.cssmin.CSSMinFilter',
 ]
 COMPRESS_CSS_HASHING_METHOD = 'content'
+
+#AWS settings 
+
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'static'
+
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
